87bb57d6236276d659cd61a9991871650aa72c2c
森篤史 <e1237@s.akashi.ac.jp>
2016-11-29 21:16:50 +0900

    test修正

--- test/test2.py
+++ test/test2.py
@@ -4,31 +4,35 @@ import numpy as np
 from hmmlearn import hmm
 
 if __name__ == '__main__':
-    states = ["Rainy", "Sunny"]
+    states = ["Start", "Rainy", "Sunny", "Finish"]
     n_states = len(states)
 
-    observations = ["walk", "shop", "clean"]
+    observations = ["start", "walk", "shop", "clean", "finish"]
     n_observations = len(observations)
 
-    start_probability = np.array([0.6, 0.4])
+    start_probability = np.array([1.0, 0, 0, 0])
 
     transition_probability = np.array([
-        [0.7, 0.3],
-        [0.4, 0.6]
+        [0, 0.6, 0.4, 0],
+        [0, 0.6, 0.2, 0.2],
+        [0, 0.3, 0.5, 0.2],
+        [0, 0, 0, 1]
     ])
 
     emission_probability = np.array([
-        [0.1, 0.4, 0.5],
-        [0.6, 0.3, 0.1]
+        [1, 0, 0, 0, 0],
+        [0, 0.1, 0.4, 0.5, 0],
+        [0, 0.6, 0.3, 0.1, 0],
+        [0, 0, 0, 0, 1]
     ])
 
-    model = hmm.MultinomialHMM(n_components=n_states)
+    model = hmm.MultinomialHMM(n_components = n_states)
     model.startprob_ = start_probability
     model.transmat_ = transition_probability
     model.emissionprob_ = emission_probability
 
     # predict a sequence of hidden states based on visible states
-    bob_says = np.array([[0, 1, 2]]).T
+    bob_says = np.array([[0, 1, 3, 2, 2, 3, 1, 4]]).T
 
 #    model = model.fit(bob_says)
     logprob, alice_hears = model.decode(bob_says, algorithm="viterbi")
